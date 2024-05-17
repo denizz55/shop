9c704e83627fda872a24e895d29a07f6df8c7273
denizz55 <lekin.den@mail.ru>
2024-05-17 21:40:59 +0300

    Начал

---
+++ app/components/Header.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+const Header = () => {
+    return(
+        <header>
+            <span>Style</span>
+            <nav>
+                <Link href="/">Главная</Link>
+            </nav>
+        </header>
+            )
+}
+
+export default Header
